38cc3ccc642fee5ae871cb97a7a903ef498f87f4
lizz <374494848@qq.com>
2020-02-20 09:27:49 +0800

    添加项目文件。

---
+++ S7NetPlusTest/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace S7NetPlusTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.地址 = new System.Windows.Forms.Label();
+            this.txt_addr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_val = new System.Windows.Forms.TextBox();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_write = new System.Windows.Forms.Button();
+            this.lbMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIp.Location = new System.Drawing.Point(85, 89);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(160, 31);
+            this.txtIp.TabIndex = 0;
+            this.txtIp.Text = "192.168.1.100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(18, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP：";
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_connect.Location = new System.Drawing.Point(284, 89);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(88, 31);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "连接";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // 地址
+            // 
+            this.地址.AutoSize = true;
+            this.地址.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.地址.Location = new System.Drawing.Point(18, 148);
+            this.地址.Name = "地址";
+            this.地址.Size = new System.Drawing.Size(73, 21);
+            this.地址.TabIndex = 3;
+            this.地址.Text = "地址：";
+            // 
+            // txt_addr
+            // 
+            this.txt_addr.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_addr.Location = new System.Drawing.Point(85, 138);
+            this.txt_addr.Name = "txt_addr";
+            this.txt_addr.Size = new System.Drawing.Size(160, 31);
+            this.txt_addr.TabIndex = 4;
+            this.txt_addr.Text = "DB1.DBW0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(20, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "数值：";
+            // 
+            // txt_val
+            // 
+            this.txt_val.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txt_val.Location = new System.Drawing.Point(85, 195);
+            this.txt_val.Name = "txt_val";
+            this.txt_val.Size = new System.Drawing.Size(160, 31);
+            this.txt_val.TabIndex = 6;
+            this.txt_val.Text = "0";
+            // 
+            // btn_read
+            // 
+            this.btn_read.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_read.Location = new System.Drawing.Point(284, 138);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(88, 31);
+            this.btn_read.TabIndex = 7;
+            this.btn_read.Text = "读取";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // btn_write
+            // 
+            this.btn_write.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_write.Location = new System.Drawing.Point(284, 195);
+            this.btn_write.Name = "btn_write";
+            this.btn_write.Size = new System.Drawing.Size(88, 31);
+            this.btn_write.TabIndex = 8;
+            this.btn_write.Text = "写入";
+            this.btn_write.UseVisualStyleBackColor = true;
+            this.btn_write.Click += new System.EventHandler(this.btn_write_Click);
+            // 
+            // lbMsg
+            // 
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.Location = new System.Drawing.Point(24, 269);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(29, 12);
+            this.lbMsg.TabIndex = 9;
+            this.lbMsg.Text = "……";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 321);
+            this.Controls.Add(this.lbMsg);
+            this.Controls.Add(this.btn_write);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.txt_val);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_addr);
+            this.Controls.Add(this.地址);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Name = "Form1";
+            this.Text = "S7NetPlusTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Label 地址;
+        private System.Windows.Forms.TextBox txt_addr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_val;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_write;
+        private System.Windows.Forms.Label lbMsg;
+    }
+}
+
---
+++ S7NetPlusTest/Form1.cs
@@ -0,0 +1,87 @@
+﻿using S7.Net;
+using S7.Net.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace S7NetPlusTest
+{
+    public partial class Form1 : Form
+    {
+        Plc _plc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_plc == null)
+                {
+                    lbMsg.Text = "连接中";
+                    _plc = new Plc(CpuType.S71200, txtIp.Text, 0, 1);
+                    _plc.Open();
+                    lbMsg.Text = "连接成功";
+                }
+                else
+                {
+                    lbMsg.Text = "断开中";
+                    _plc.Close();
+                    _plc = null;
+                    lbMsg.Text = "断开成功";
+                }
+            }
+            catch (PlcException ex)
+            {
+                lbMsg.Text = ex.Message;
+            }
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbMsg.Text = "读取中";
+
+                List<DataItem> dataItems = new List<DataItem>();
+                var item = DataItem.FromAddress(txt_addr.Text);
+                dataItems.Add(item);
+
+                _plc.ReadMultipleVars(dataItems);
+
+                var result = dataItems.First().Value.ToString();
+
+                lbMsg.Text = result;
+            }
+            catch (PlcException ex)
+            {
+                lbMsg.Text = ex.Message;
+            }
+
+        }
+
+        private void btn_write_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbMsg.Text = "写入中";
+
+                List<DataItem> dataItems = new List<DataItem>();
+                var item = DataItem.FromAddressAndValue<string>(txt_addr.Text, txt_val.Text);
+                dataItems.Add(item);
+
+                _plc.Write(dataItems.ToArray());
+
+                lbMsg.Text = "写入成功";
+            }
+            catch (PlcException ex)
+            {
+                lbMsg.Text = ex.Message;
+            }
+
+        }
+    }
+}
